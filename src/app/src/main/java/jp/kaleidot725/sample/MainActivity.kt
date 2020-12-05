e2cc05669e59f3b3adfb7c300a845e60e0b0a822
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-05 22:49:27 +0900

    feat: add new dialog fragment

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -7,9 +7,35 @@ import android.widget.Button
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val normalDialogFragmentButton = findViewById<Button>(R.id.normal_dialog_fragment_button)
+        normalDialogFragmentButton.setOnClickListener {
+            NormalDialogFragment().show(supportFragmentManager, "")
+        }
+
         val fullScreenDialogButton = findViewById<Button>(R.id.fullscreen_dialog_fragment_button)
         fullScreenDialogButton.setOnClickListener {
             FullScreenDialogFragment().show(supportFragmentManager, "")
+        }
+
+        val backgroundDialogFragmentButton = findViewById<Button>(R.id.background_dialog_fragment)
+        backgroundDialogFragmentButton.setOnClickListener {
+            BackgroundDialogFragment().show(supportFragmentManager, "")
+        }
+
+        val positionDialogFragmentButton = findViewById<Button>(R.id.position_dialog_fragment)
+        positionDialogFragmentButton.setOnClickListener {
+            PositionDialogFragment().show(supportFragmentManager, "")
+        }
+
+        val roundedDialogFragmentButton = findViewById<Button>(R.id.rounded_dialog_fragment)
+        roundedDialogFragmentButton.setOnClickListener {
+            RoundedDialogFragment().show(supportFragmentManager, "")
+        }
+
+        val popUpDialogFragmentButton = findViewById<Button>(R.id.popup_dialog_fragment)
+        popUpDialogFragmentButton.setOnClickListener {
+            PopUpDialogFragment().show(supportFragmentManager, "")
         }
     }
 }
---
+++ src/app/src/main/java/jp/kaleidot725/sample/NormalDialogFragment.kt
@@ -0,0 +1,17 @@
+package jp.kaleidot725.sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class NormalDialogFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_fragment_full_screen, container,  false)
+    }
+}
